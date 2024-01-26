98cddd71f98fe9a3aa535fa265ce8dd246b0cffb
Blake <blake@singtecs.com>
2024-01-27 03:25:59 +0630

    feat: patient type add

---
+++ src/dataTypes/patient.ts
@@ -0,0 +1,15 @@
+export type PATIENT = {
+  cMID?: string;
+  cargoType?: string;
+  hJobID: string;
+  marks?: string;
+  qty?: number;
+  description?: string;
+  packStyle?: string;
+  weight?: number;
+  measurement?: number;
+  createdBy?: string;
+  createdAt?: Date;
+  modifiedBy?: string;
+  modifiedAt?: Date;
+};
